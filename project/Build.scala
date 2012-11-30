bd1d122e6091d19728c3302ba062192bdf5f2b2b
jtournay <jtournay@linkedin.com>
2012-11-30 06:39:21 -0800

    dsgn

--- project/Build.scala
+++ project/Build.scala
@@ -8,14 +8,13 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+    // Add your project dependencies here
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    incrementalAssetsCompilation := false
   )
 
 }
